0a5c263201c6d701ebaf356fa3bb5f045a3d5e4b
Alberto Zerbinati <albz@bendingspoons.com>
2023-09-17 20:04:18 +0000

    define header

--- include/people-detection/PeopleDetector.hpp
+++ include/people-detection/PeopleDetector.hpp
@@ -1,23 +1,35 @@
+// Alberto Zerbinati
+
 #ifndef PEOPLE_DETECTOR_HPP
 #define PEOPLE_DETECTOR_HPP
 
-#include <torch/script.h>  // One-stop header for LibTorch
+#include <torch/torch.h>
 
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+
+struct DetectedWindow {
+    int x, y, w, h;
+    float confidence;
+};
 
 class PeopleDetector {
    public:
     PeopleDetector(const std::string& model_path);
-    ~PeopleDetector();
-
-    void LoadModel();
-    void ReadImage(const std::string& image_path);
-    void SlidingWindowApproach(int window_size, int step_size);
+    std::vector<DetectedWindow> detectPeople(const cv::Mat& image);
 
    private:
-    std::string model_path_;
     torch::jit::script::Module model_;
-    cv::Mat input_image_;
+
+    void loadModel(const std::string& model_path);
+    bool ImageToTensor(cv::Mat& image, torch::Tensor& tensor);
+    std::vector<DetectedWindow> performSlidingWindow(const cv::Mat& image,
+                                                     float scale);
+    std::pair<std::vector<int>, float> performKMeans(
+        const std::vector<cv::Point2f>& centers, int k);
+    float computeWeightedLoss(float kmeansLoss, float scale, int k, float gamma,
+                              float delta);
 };
 
 #endif  // PEOPLE_DETECTOR_HPP
