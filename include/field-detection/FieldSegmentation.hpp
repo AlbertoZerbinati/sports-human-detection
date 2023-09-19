b200cb4dd6b5be88396ff0e3abd1711f0437a649
Alberto Zerbinati <albz@bendingspoons.com>
2023-09-19 18:17:25 +0000

    fix: remove header

--- include/field-detection/FieldSegmentation.hpp
+++ include/field-detection/FieldSegmentation.hpp
@@ -1,13 +1,17 @@
 // Marco Calì
 
-#include "field-detection/GreenFieldSegmentation.hpp"
+// TODO: This causes make to fail...
+// I don't know why but anyway this code seriously needs refactoring
+// #include "field-detection/GreenFieldSegmentation.hpp"
 
 // TODO: make a class...
 
 // TODO: rename to lowercase...
-Mat GreenFieldsSegmentation(const Mat &I);
-Mat GenericFieldSegmentation(const Mat &image,
-                             const Vec3b estimated_field_color,
-                             double mean_factor = 1, double std_factor = 1);
-Mat ColorFieldSegmentation(const Mat &src, const Vec3b estimated_field_color);
-Mat FieldSegmentation(const Mat &src, const Vec3b estimated_field_color);
+cv::Mat GreenFieldsSegmentation(const cv::Mat &I);
+cv::Mat GenericFieldSegmentation(const cv::Mat &image,
+                                 const cv::Vec3b estimated_field_color,
+                                 double mean_factor = 1, double std_factor = 1);
+cv::Mat ColorFieldSegmentation(const cv::Mat &src,
+                               const cv::Vec3b estimated_field_color);
+cv::Mat FieldSegmentation(const cv::Mat &src,
+                          const cv::Vec3b estimated_field_color);
