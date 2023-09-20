61035f80609d0bcd4091b4d78990bd9204c8c8d6
Alberto Zerbinati <albz@bendingspoons.com>
2023-09-20 20:33:06 +0000

    main save files and show results

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/opencv.hpp>
 
 #include "pipeline/Pipeline.hpp"
+#include "utils/Utils.hpp"
 
 int main(int argc, char** argv) {
     if (argc != 5) {
@@ -27,26 +28,33 @@ int main(int argc, char** argv) {
     // Run the pipeline
     PipelineRunOutput runOutput = pipeline.run();
 
+    std::cout << "\nRun completed! Evaluating results..." << std::endl;
+
     // extract the file name from the path
     std::string img_name = argv[1];
     img_name = img_name.substr(img_name.find_last_of("/") + 1);
     img_name = img_name.substr(0, img_name.find_last_of("."));
 
+    // Evaluate the pipeline output
+    PipelineEvaluateOutput evalOutput = pipeline.evaluate(runOutput);
+
+    // Print the evaluation metrics
+    std::cout << "\n  - mIoU: " << evalOutput.mIoU << std::endl;
+    std::cout << "  - mAP: " << evalOutput.mAP << std::endl;
+
+    std::cout << "\nSaving outputs to files: " << img_name + "_bin_mask.png"
+              << ", " << img_name + "_color_mask.png"
+              << ", " << img_name + "_bboxes.txt" << std::endl;
+
     // Save the outputs
     cv::imwrite(img_name + "_bin_mask.png", runOutput.segmentationBinMask);
     cv::imwrite(img_name + "_color_mask.png", runOutput.segmentationColorMask);
-
-    // TODO: write the bounding boxes to file
-
-    // Evaluate the pipeline (if needed)
-    PipelineEvaluateOutput evalOutput = pipeline.evaluate(runOutput);
-
-    // Output results (for demonstration)
-    std::cout << "\nNumber of detected players: "
-              << runOutput.boundingBoxes.size() << std::endl;
-
-    // Add your code to display or save results
-    // ...
+    Utils::writeBoundingBoxesToFile(
+        runOutput.boundingBoxes,
+        img_name +
+            "_bboxes.txt");  // TODO: here there are some players returned by
+                             // the pipeline that are assigned to team -1 ...
+                             // manage this throughout the pipeline and here
 
     return 0;
 }
