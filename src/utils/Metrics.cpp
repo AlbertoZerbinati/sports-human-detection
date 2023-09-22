f6710b6604e7af17338c63765034e952cb62806e
Sedu999 <145329830+Sedu999@users.noreply.github.com>
2023-09-22 11:48:06 +0200

    Add files via upload

--- src/utils/Metrics.cpp
+++ src/utils/Metrics.cpp
@@ -1,12 +1,17 @@
-// To-be-splitted
+// mIoU Marco Sedusi
+// Map Marco Calì
+
 
 #include "utils/Metrics.hpp"
-
 #include <opencv2/core/core.hpp>
 #include <vector>
-
 #include "utils/Utils.hpp"
 
+
+/* calculateClassIoU
+ * Parameters: predicted is the matrix segmented returned by our algorithm
+ * groundTruth is reference matrix segmented
+ * label is an interger value to indicate the class for which we want to compute the IoU metric */
 float MetricsEvaluator::calculateClassIoU(const cv::Mat &predicted,
                                           const cv::Mat &groundTruth,
                                           int label) {
@@ -15,13 +20,15 @@ float MetricsEvaluator::calculateClassIoU(const cv::Mat &predicted,
 
     for (int i = 0; i < predicted.rows; i++) {
         for (int j = 0; j < predicted.cols; j++) {
-            // both true
+            // Both true
+            // If the two masks have same value then counter++ for union and intersection
             if (predicted.at<uchar>(i, j) == label &&
                 groundTruth.at<uchar>(i, j) == label) {
                 intersect++;
                 uni++;
             }
-            // only one true
+            // Only one true
+            // If only one of the two masks have the specified value label then counter++ for union
             else if (predicted.at<uchar>(i, j) == label &&
                          groundTruth.at<uchar>(i, j) != label ||
                      predicted.at<uchar>(i, j) != label &&
@@ -30,16 +37,19 @@ float MetricsEvaluator::calculateClassIoU(const cv::Mat &predicted,
             }
         }
     }
-
+	// To avoid division by zero
     if (uni == 0) {
         uni = 1;
     }
-
-    return (float)intersect / uni;
+	// Return the IoU for one class
+    return (float)intersect / uni;  
 }
-
+/* calculateMIoU
+ * Parameters: predicted is the matrix segmented returned by our algorithm
+ * groundTruth is reference matrix segmented */
 float MetricsEvaluator::calculateMIoU(const cv::Mat &predicted,
                                              const cv::Mat &groundTruth) {
+	// Two matrices must have same size and type 
     if (!(predicted.size() == groundTruth.size() &&
           predicted.type() == groundTruth.type())) {
         std::cerr
@@ -47,11 +57,12 @@ float MetricsEvaluator::calculateMIoU(const cv::Mat &predicted,
             << std::endl;
         return 0;
     }
-
+	// Perform the metric computation for the 3 classes (field,team1 and team2)
     float IoU1 = calculateClassIoU(predicted, groundTruth, 1);
     float IoU2 = calculateClassIoU(predicted, groundTruth, 2);
     float IoU3 = calculateClassIoU(predicted, groundTruth, 3);
-
+	
+	// Return the mIoU as float
     return (IoU1 + IoU2 + IoU3) / 3;
 }
 
